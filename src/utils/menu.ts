f168f8fa7dd07aece7b483e601f5cecc3c2598e7
Дмитрий Шейко <sheydim@gmail.com>
2022-12-24 22:10:34 +0300

    Fix icon paths

--- src/utils/menu.ts
+++ src/utils/menu.ts
@@ -1,8 +1,8 @@
-import discover from '../../assets/discover.svg';
-import live from '../../assets/live.svg';
-import shows from '../../assets/shows.svg';
-import movies from '../../assets/movies.svg';
-import stuff from '../../assets/stuff.svg';
+import discover from '../assets/discover.svg';
+import live from '../assets/live.svg';
+import shows from '../assets/shows.svg';
+import movies from '../assets/movies.svg';
+import stuff from '../assets/stuff.svg';
 
 export const menu = [
   { id: 1, title: 'Discover', icon: discover },
