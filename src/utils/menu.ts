58ea3ed2cb8a157ce80a853ca90924b4839d533f
Дмитрий Шейко <sheydim@gmail.com>
2022-12-24 22:08:14 +0300

    Create menu items array

---
+++ src/utils/menu.ts
@@ -0,0 +1,13 @@
+import discover from '../../assets/discover.svg';
+import live from '../../assets/live.svg';
+import shows from '../../assets/shows.svg';
+import movies from '../../assets/movies.svg';
+import stuff from '../../assets/stuff.svg';
+
+export const menu = [
+  { id: 1, title: 'Discover', icon: discover },
+  { id: 2, title: 'Live TV', icon: live },
+  { id: 3, title: 'TV Shows', icon: shows },
+  { id: 4, title: 'Movies', icon: movies },
+  { id: 5, title: 'My Stuff', icon: stuff },
+];
