fee54c2afaf59e99acf177e33216739104acf17c
Дмитрий Шейко <sheydim@gmail.com>
2022-12-25 17:31:34 +0300

    Add basic logic

--- src/components/CardContainer/CardContainer.tsx
+++ src/components/CardContainer/CardContainer.tsx
@@ -1,7 +1,17 @@
-import styles from './CardContainer.module.css'
+import { Item } from '../../types/IData';
+import { Card } from '../Card/Card';
+import styles from './CardContainer.module.css';
 
-export const CardContainer = () => {
-  return (
-    <div>CardContainer</div>
-  )
+interface CardContainerProps {
+  content: Item[];
 }
+
+export const CardContainer = ({ content }: CardContainerProps) => {
+  return (
+    <div>
+      {content.map(item => (
+        <Card item={item} />
+      ))}
+    </div>
+  );
+};
