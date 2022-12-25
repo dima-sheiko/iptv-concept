e0c69628c4b8eb1a6f80f0a222e7041e16ee3b97
Дмитрий Шейко <sheydim@gmail.com>
2022-12-25 18:38:41 +0300

    Add Card Container

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,25 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { Background } from './components/Background/Background';
 import { IData } from './types/IData';
 import axios from 'axios';
 import './styles/styles.css';
 import { Header } from './components/Header/Header';
 import { Search } from './components/Search/Search';
+import { CardContainer } from './components/CardContainer/CardContainer';
 
 export const App = () => {
   const [data, setData] = useState<IData[]>([]);
   const [query, setQuery] = useState('');
 
-  const backgrounds = data.flatMap(item => item.backgrounds.map(value => value.url));
-  const items = data.flatMap(item => item.items.map(value => value));
-  const content = items.filter(item => item.title.toLowerCase().includes(query.toLowerCase()));
+  const backgrounds = useMemo(() => data.flatMap(item => item.backgrounds.map(value => value.url)), [data]);
+  const items = useMemo(() => data.flatMap(item => item.items.map(value => value)), [data]);
+  const content = useMemo(
+    () => items.filter(item => item.title.toLowerCase().includes(query.toLowerCase())),
+    [items, query]
+  );
 
   // API call imitation
   useEffect(() => {
@@ -39,6 +43,7 @@ export const App = () => {
         <Header />
         <Search setQuery={setQuery} />
         {/* <h3>{!query ? 'In spotlight' : content.length === 0 ? '' : `TV Shows (${content.length})`}</h3> */}
+        <CardContainer content={content} />
       </div>
     </>
   );
