00956f16fcfae6eb048be25fe97a6b4ec1824dd4
Дмитрий Шейко <sheydim@gmail.com>
2022-12-25 23:12:31 +0300

    Fix imports

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ import '@fontsource/roboto/700.css';
 import { useState, useEffect, useMemo } from 'react';
 import { Background } from './components/Background/Background';
 import { IData } from './types/IData';
-import axios from 'axios';
-import './styles/styles.css';
 import { Header } from './components/Header/Header';
 import { Search } from './components/Search/Search';
 import { CardContainer } from './components/CardContainer/CardContainer';
+import axios from 'axios';
+import './styles/styles.css';
 
 export const App = () => {
   const [data, setData] = useState<IData[]>([]);
