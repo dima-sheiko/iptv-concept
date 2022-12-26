73d23ef4868971ee1b80fe53e2f151a9b44d3fb4
Дмитрий Шейко <sheydim@gmail.com>
2022-12-26 12:43:16 +0300

    Add ghpages support

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/netup-task/',
   plugins: [react(), checker({ typescript: true })],
 });
