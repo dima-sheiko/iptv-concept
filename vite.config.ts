c600bf3d13ac1183009d127df702de5daa7ac81c
Дмитрий Шейко <sheydim@gmail.com>
2023-01-12 13:57:45 +0300

    Add copy plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import checker from 'vite-plugin-checker';
+import copy from 'rollup-plugin-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/netup-task/',
-  plugins: [react(), checker({ typescript: true })],
+  plugins: [
+    react(),
+    checker({ typescript: true }),
+    copy({
+      targets: [
+        {
+          src: './api',
+          dest: 'dist/api',
+        },
+      ],
+      hook: 'writeBundle',
+    }),
+  ],
 });
