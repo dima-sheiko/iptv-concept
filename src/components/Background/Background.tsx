97b6a844c84f73ea5a0e61322ee0f0439595eeeb
Дмитрий Шейко <sheydim@gmail.com>
2023-01-12 14:05:55 +0300

    Add optimization

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -1,16 +1,19 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
+import { IURL } from '../../types/types';
 import styles from './Background.module.css';
 
 interface BackgroundProps {
-  backgrounds: string[];
+  backgrounds: IURL[];
 }
 
 export const Background = ({ backgrounds }: BackgroundProps) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
+  const background = useMemo(() => backgrounds.map(item => item.url), [backgrounds]);
+
   useEffect(() => {
     const intervalId = setInterval(() => {
-      if (currentIndex === backgrounds.length - 1) {
+      if (currentIndex === background.length - 1) {
         setCurrentIndex(0);
       } else {
         setCurrentIndex(currentIndex + 1);
@@ -24,8 +27,8 @@ export const Background = ({ backgrounds }: BackgroundProps) => {
     <div
       className={styles.bg}
       style={{
-        backgroundImage: `url(${backgrounds[currentIndex]})`,
+        backgroundImage: `url(${background[currentIndex]})`,
       }}
-    ></div>
+    />
   );
 };
