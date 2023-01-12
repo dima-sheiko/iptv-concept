eda68b70014042d2ba52d6113c13a9be46adc152
Дмитрий Шейко <sheydim@gmail.com>
2023-01-12 14:05:12 +0300

    Change API fetching approach

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,37 @@ import '@fontsource/ubuntu/';
 import '@fontsource/source-sans-pro';
 import { useState, useEffect, useMemo } from 'react';
 import { Background } from './components/Background/Background';
-import { IData } from './types/IData';
 import { Header } from './components/Header/Header';
 import { Search } from './components/Search/Search';
 import { CardContainer } from './components/CardContainer/CardContainer';
 import { Divider } from './components/Divider/Divider';
-import { ActionsEnum, HttpHandler } from './utils/HttpHandler';
+import { IURL, Item } from './types/types';
 import axios from 'axios';
 import './styles/styles.css';
 
 export const App = () => {
-  const [data, setData] = useState<IData[]>([]);
+  const [backgrounds, setBackgrounds] = useState<IURL[]>([]);
+  const [items, setItems] = useState<Item[]>([]);
   const [error, setError] = useState('');
   const [query, setQuery] = useState('');
 
-  const backgrounds = useMemo(() => data.flatMap(item => item.backgrounds.map(value => value.url)), [data]);
-  const items = useMemo(() => data.flatMap(item => item.items.map(value => value)), [data]);
   const content = useMemo(
     () => items.filter(item => item.title.toLowerCase().includes(query.toLowerCase())),
     [items, query]
   );
 
-  // API call imitation
   useEffect(() => {
     const getData = async () => {
       try {
-        const response = await HttpHandler.get(ActionsEnum.GET_DISCOVER);
-        setData(response);
+        const { data } = await axios.get('api/discover');
+        setBackgrounds(data.backgrounds.map((background: IURL) => ({ ...background, url: `/api/${background.url}` })));
+        setItems(
+          data.items.map((item: Item) => ({
+            ...item,
+            poster: `/api/${item.poster}`,
+            keyframe: `/api/${item.keyframe}`,
+          }))
+        );
       } catch (error) {
         if (axios.isAxiosError(error)) {
           setError(`Error: ${error.message}`);
